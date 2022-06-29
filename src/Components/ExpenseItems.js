5305cc743a543f8d85ac14b505dd494fb1182494
Zuber-GitHub <zuberriyazahmed@gmail.com>
2022-06-29 13:27:00 +0530

    ExpenseItem Added

---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,15 @@
+function ExpenseItem()
+{
+    return  <div>
+        <h2> ExpenseItems are listed below</h2>
+        <pre>
+            <h3>Food Rs 10</h3>
+            <h3>Petrol Rs 100</h3>
+            <h3>Movies Rs 200</h3>
+        </pre>
+
+    </div>
+    
+}
+
+export default ExpenseItem;

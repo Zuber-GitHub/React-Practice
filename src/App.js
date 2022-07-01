4872bd5fa73278cb33160f8121b8c5bd773b1552
Zuber-GitHub <zuberriyazahmed@gmail.com>
2022-07-01 12:57:59 +0530

    EventHandlers Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -34,7 +35,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
